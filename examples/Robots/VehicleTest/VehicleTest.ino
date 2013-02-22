c496e8b96531f70bd7e5d46acd18e45484abb85b
Xun Yang <me@xunyangixder.com>
2013-02-22 16:17:56 +0100

    Remove description from Robots week, for wiki.

--- examples/Robots/VehicleTest/VehicleTest.ino
+++ examples/Robots/VehicleTest/VehicleTest.ino
@@ -1,11 +1,3 @@
-/*  Test for example "Vechile"
-*
-*  Use this sketch if example Vechile does not work
-*  properly. It allows you to get the reading of 
-*  left and right LDR, so you can change the baseValue
-*  and threshold of Vechile example accordingly.
-*
-*/
 #include <Castilla.h>
 
 //Declare the left and right LDR sensor
